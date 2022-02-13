ff2bca8de63ee96b8a056f9621213b0b1d3733fb
Yousif Shafee <yousiftariq1@hotmail.com>
2022-02-13 11:52:58 +0200

    Complete Project Requirements

--- server.js
+++ server.js
@@ -1,18 +1,33 @@
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer');
 require('dotenv').config()
 
 var app = express();
-
+var upload = multer({ dest: 'upload/' });
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+var type = upload.single('upfile');
+
 app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', type, (req, res) => {
+  var upload_file = req.file
+  try {
+    res.json(
+      { 
+        "name": upload_file.originalname,
+        "type": upload_file.mimetype,
+        "size": upload_file.size 
+      }
+    )
+  } catch {
+    res.json({error : "There Is Error"})
+  }
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
